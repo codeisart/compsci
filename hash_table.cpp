71db9cb571afe9983105b3981f7db57783365279
Jimmy Smith <jimmy@doomkeeper.com>
2012-07-18 13:41:06 -0700

    started adding tri class

---
+++ hash_table.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+template<typename KEY=const char*,typename VALUE=int>
+class HashTable
+{
+private:
+	typedef unsigned int Hash;
+	typedef KEY Key;
+	typedef VALUE Value;
+	typedef std::pair<Hash,Value> Kvp;
+	typedef std::vector<Kvp> HashContainer;
+
+	Hash generateHash(const char* v)	// string hash.
+	{
+		Hash hash = 2166136261U;		// offset.
+		const Hash fnvPrime = 16777619;	// fnv prime
+		while(Hash c = *v++)				// assume string for now.	
+		{
+			hash ^= c;
+			hash *= fnvPrime;
+		}
+		return hash;
+	}
+
+	HashContainer m_hashSpace;
+public:
+	
+	HashTable(int tableSize = 251)	// Biggest prime < 256
+	{
+		m_hashSpace.resize(tableSize);
+	}
+	bool add(Key k,Value v)
+	{
+		Hash hash = generateHash(k);
+		int index = hash % m_hashSpace.size();
+
+		Kvp kvp(hash,v);
+		const int maxAttempts = m_hashSpace.size();
+		
+		for(int i = 0; i < maxAttempts && m_hashSpace[index].first!=0 && m_hashSpace[index].first != hash; i++)
+			index = (index+1) % m_hashSpace.size();
+		
+		if(m_hashSpace[index].first == 0 || m_hashSpace[index].first == hash)
+		{
+			m_hashSpace[index] = kvp;
+			return true;		
+		}	
+		return false;	
+	}	
+	void print()
+	{	
+		using namespace std;	
+		//for( HashContainer::iterator i = m_hashSpace.begin(); i != m_hashSpace.end(); ++i) 
+		for(int i = 0; i < m_hashSpace.size(); ++i)
+		{		
+			const Kvp& thing = m_hashSpace[i];
+			if(thing.first)	
+			{
+				cout << setw(10) << "Index: " << i;
+				cout << setw(10) << "Hash: " << thing.first;
+				cout << setw(10) << "Value: " << thing.second;
+				cout << endl;
+			}
+		}
+	}
+};
+
+
+int main(int argc, char** argv)
+{	
+	using namespace std;
+
+	HashTable<> table;
+	table.add("fire",5);
+	table.add("walk",9);
+	table.add("with",2);
+	table.add("me",6);
+	table.print();
+	
+
+	return 0;
+}
---
+++ tri.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Tri
+{
+	struct Node
+	{
+		Node(char c=0)  : data(c) {}
+		typedef vector<Node*> ChildCon; 
+		ChildCon children;
+		char data;
+
+		Node* findChild(char c) const
+		{
+			// Crappy linear search. Replace with binary search.		
+			for(ChildCon::const_iterator i = children.begin(); i != children.end(); ++i)
+				if((*i)->data == c)
+					return *i;
+			return 0;
+		}
+		Node* insertChild(char c)
+		{
+			children.push_back(new Node(c));
+			return children.back();
+		}
+	};
+	Node* m_root;
+
+public:
+
+	Tri() 
+		: m_root(new Node()) {}
+	
+	void insert(const string& str)
+	{
+		cout << "inserting " << str << endl;
+		Node* node = m_root;
+		for(string::const_iterator i = str.begin(); i!= str.end(); ++i)
+		{
+			// Decend until we can't any more.
+			Node* n = node->findChild(*i);
+			if(n)
+				node = n;
+			else
+				n = node->insertChild(*i);
+
+			// Insefficent here, should really fail and stop searching as these are new nodes with no children.
+			node = n;
+		}
+	}
+};
+
+int main(int argc, char** argv)
+{
+	Tri t;
+
+	string s;
+	while(cin >> s)
+	{
+		//cout << s << endl;
+		t.insert(s);
+	}
+
+
+	return 0;
+}
