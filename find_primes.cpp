41c369d973502437345a925f496f60542ad49a75
Jimmy Smith <jimmy@doomkeeper.com>
2012-08-01 21:59:33 -0400

    added mt version

--- find_primes.cpp
+++ find_primes.cpp
@@ -5,6 +5,8 @@
 #include <future>
 
 typedef std::vector<int> Col;
+typedef std::future<Col> FutureCol;
+typedef std::vector<FutureCol> Results;
 
 template<typename ITER>
 inline bool isPrime(int n, ITER begin, ITER end)
@@ -16,41 +18,66 @@ inline bool isPrime(int n, ITER begin, ITER end)
 	);
 
 	return divisor == end;
+}
 
-	//for(Col::const_iterator i = primes.begin(); i!= primes.end(); ++i)
-	//	if(n % *i == 0)
-	//		return false;		
-	//return true;
+Col findPrimes(int rangeStart, int rangeEnd, const Col& earlyPrimes)
+{
+	Col newlyFound;
+	if(!rangeStart & 1)
+		rangeStart++;
+
+	for(int i = rangeStart; i < rangeEnd; i+=2)
+		if(isPrime(i,earlyPrimes.begin(), earlyPrimes.end()))
+			newlyFound.push_back(i);
+
+	return newlyFound;
 }
 
 int main(int argc, char** argv)
 {
-	using namespace std;
-	vector<int> primes;
+	static const size_t earlyPrimesNum = 200;
+	static const size_t primesSearchRange = 1000;
 
-	int toFind = 100;
+	using namespace std;
+	Col earlyPrimes;
+	earlyPrimes.reserve(earlyPrimesNum);
+
+	int toFind = earlyPrimesNum;
 
 	if(argc>1)
 		std::stringstream(argv[1]) >> toFind;
 
 	std::cout << "looking for " << toFind << " primes" << std::endl;
 
+	// Stage1 build early prime lookup.
 	int i = 3;
-	primes.push_back(2);
-	while(primes.size() < toFind)
+	earlyPrimes.push_back(2);
+	while(earlyPrimes.size() < earlyPrimesNum)
 	{
-		int n = std::min(primes.size(),(size_t)200);
-		if(isPrime(i,primes.begin(),primes.begin()+n))
+		int n = std::min(earlyPrimes.size(),earlyPrimesNum);
+		if(isPrime(i,earlyPrimes.begin(),earlyPrimes.begin()+n))
 		{
 			std::cout << i << std::endl;
 			std::cerr << i << std::endl;
-			primes.push_back(i);
+			earlyPrimes.push_back(i);
 		}
 		i+=2;
 		if(i % 100)
 			std::cerr << ".";
 	}
 
+	Col mergedResults(earlyPrimes);
+	mergedResults.reserve(toFind);
+	Results results;
+
+	//  Stage2 dispatch multiple threads.
+	for(int i = earlyPrimesNum; i; i+= primesSearchRange)
+	{
+		results.push_back(
+			std::async([i,earlyPrimes] { return findPrimes(i,i+primesSearchRange,earlyPrimes); })
+		);
+	}
+	
 	return 0;
 }
 
